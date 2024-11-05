030bf385c5b65a6c396e39998d441e02719c898e
thespekky <bandras123456@gmail.com>
2024-11-05 21:24:30 +0100

    Add project files.

---
+++ Black Friday/Model/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Black_Friday.Model
+{
+    internal class Item
+    {
+        private string Nev { get; set; }
+        private string Gyarto { get; set; }
+        private int Ar { get; set; }
+        private int Szorzo { get; set; }
+        private int Akcios_ar { get; set; }
+
+        public Item(string nev,string gyaroto,int ar,int szorzo)
+        {
+            this.Nev = nev;
+            this.Gyarto = gyaroto;
+            this.Ar = ar;
+            if (szorzo <= 100 && szorzo <= 1)
+            {
+                this.Szorzo = szorzo;
+                this.Akcios_ar = (this.Ar * this.Szorzo) / 100;
+            }
+        }
+
+
+    }
+}
---
+++ Black Friday/View/Black Friday.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Black_Friday
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form form2 = new Form();
+            form2.Text = "Valami";
+            Button b = new Button();
+            b.BackColor = Color.Red;
+            b.Location = new Point(100, 100);
+            b.Show();
+           
+            form2.ShowDialog();
+        }
+    }
+}
